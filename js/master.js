62818ff278fd93b12caa2edc84deaef1dd571c88
Grayson Croom <grayson.croom@gmail.com>
2016-12-20 18:58:12 -0600

    Update master.js

--- js/master.js
+++ js/master.js
@@ -3,14 +3,14 @@ var diyButton = document.querySelector("a");
 var count = 1;
 var MAXPAGES = 5;
 
-header.style.backgroundImage = "url(pictures/background0.jpg";
+header.style.backgroundImage = "url(./pictures/background0.jpg";
 
 // makes the text in the center of landing page, change the header image when clicked
 diyButton.addEventListener("click", cycleHeaderImage);
 
 function cycleHeaderImage(){
 	if(count < MAXPAGES) {
-		header.style.backgroundImage = "url(pictures/background" + count + ".jpg)";
+		header.style.backgroundImage = "url(./pictures/background" + count + ".jpg)";
 		count++;
 	}
 	else count = 0;
