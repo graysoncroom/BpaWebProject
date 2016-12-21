5ddaf6965f22fa8d2d6adee3ecac28eaf7a6c551
Grayson Croom <grayson.croom@gmail.com>
2016-12-21 02:57:15 -0600

    Update

--- js/master.js
+++ js/master.js
@@ -1,12 +1,41 @@
 var header = document.querySelector("header");
-var diyButton = document.querySelector("a");
+var landingButtons = document.querySelectorAll("a");
 var count = 1;
 var MAXPAGES = 5;
 
-header.style.backgroundImage = "url(pictures/background0.jpg";
+function smoothScrollToProject(){smoothScroll(document.querySelector("#proj"));}
+function smoothScrollToTeam(){smoothScroll(document.querySelector("#team"));}
+function smoothScrollToBts(){smoothScroll(document.querySelector("#bts"));}
+function smoothScrollToLand(){smoothScroll(document.querySelector("header"));}
 
-// makes the text in the center of landing page, change the header image when clicked
-diyButton.addEventListener("click", cycleHeaderImage);
+function smoothScroll(target){
+	var scrollContainer = target;
+	var targetY = 0;
+    do {
+        scrollContainer = scrollContainer.parentNode;
+        if (!scrollContainer) return;
+        scrollContainer.scrollTop += 1;
+    } while (scrollContainer.scrollTop == 0);
+    do {
+        if (target == scrollContainer) break;
+        targetY += target.offsetTop;
+    } while (target = target.offsetParent);
+    function scroll(c, a, b, i) {
+        i++;
+		if (i > 30)
+			return;
+        c.scrollTop = a + (b - a) / 30 * i;
+        setTimeout(function(){ scroll(c, a, b, i); }, 30);
+    }
+    scroll(scrollContainer, scrollContainer.scrollTop, targetY, 0);
+}
+
+function init(){
+	header.style.backgroundImage = "url(pictures/background0.jpg)";
+	landingButtons[0].addEventListener("click", smoothScrollToProject);
+	landingButtons[1].addEventListener("click", smoothScrollToTeam);
+	landingButtons[2].addEventListener("click", smoothScrollToBts);
+}
 
 function cycleHeaderImage(){
 	if(count < MAXPAGES) {
@@ -15,3 +44,5 @@ function cycleHeaderImage(){
 	}
 	else count = 0;
 }
+
+init();
