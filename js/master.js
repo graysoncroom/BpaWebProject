b367a6222fcf7e478e67868c6d34ae6cb8484437
Grayson Croom <grayson.croom@gmail.com>
2016-12-20 18:53:21 -0600

    switch background image with the button below title

--- js/master.js
+++ js/master.js
@@ -1,16 +1,16 @@
 var header = document.querySelector("header");
-var diyButton = document.querySelector("h1");
+var diyButton = document.querySelector("a");
 var count = 1;
 var MAXPAGES = 5;
 
-header.style.backgroundImage = "url(pictures/background0.jpg";
+header.style.backgroundImage = "url(/pictures/background0.jpg";
 
 // makes the text in the center of landing page, change the header image when clicked
 diyButton.addEventListener("click", cycleHeaderImage);
 
 function cycleHeaderImage(){
 	if(count < MAXPAGES) {
-		header.style.backgroundImage = "url(pictures/background" + count + ".jpg)";
+		header.style.backgroundImage = "url(/pictures/background" + count + ".jpg)";
 		count++;
 	}
 	else count = 0;
