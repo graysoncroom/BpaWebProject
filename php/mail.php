b3a044c1709152276f862575436201aef6fc5906
Grayson Croom <grayson.croom@gmail.com>
2017-01-02 14:52:42 -0600

    changes

--- php/mail.php
+++ php/mail.php
@@ -11,6 +11,8 @@
 	$mailheader = "From: $email \r\n";
 
 	mail($recipient, $subject, $formcontent, $mailheader);
+	mail($email. "Form Submision Notice", "Your information request has been sucessfully sent!", $mailheader);
+
 
 	header('Location: ../index.html#bts');
 ?>
